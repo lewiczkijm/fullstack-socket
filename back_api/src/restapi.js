e1cbd5e8e30cd6bdaa2883fc9c2e95fcf93632d5
mikhaillevitskiidev <mikhaillevitskiidev@gmail.com>
2022-04-06 00:22:28 +0300

    add endpoint to get user by id

--- back_api/src/restapi.js
+++ back_api/src/restapi.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const router = express.Router()
+const {ObjectId} = require("mongodb")
 // router.use(function (req, res, next) {
 //     next();
 // });
@@ -11,17 +12,41 @@ function restApi(db){
 
     // Login function: get name, send objectId
     router.post("/login",async (req,res)=>{
+
         if(!req.body.name) {
             res.status(500).send("Name didn't get")
             return
         }
+
         const employee = await collection.findOne({name:req.body.name})
+
         if(employee === null) {
             res.status(404).send("Employee not found")
             return
         }
         res.send(employee._id)
     })
+
+    router.get("/user/:id",async (req,res)=>{
+        let id
+
+        try{
+            id = new ObjectId(req.params.id)
+        } catch (e){
+            res.status(500).send("Incorrect user id")
+            return
+        }
+
+        const employee = await collection.findOne({"_id":id})
+        console.log(employee)
+        if(employee === null) {
+            res.status(404).send("Employee not found")
+            return
+        }
+
+        res.send(employee)
+    })
+
     return router
 }
 
